430e27b20ebfec6549192d3eed3f37e7cabcecde
Kelyane <kelyane.lokita@gmail.com>
2015-10-17 18:02:28 +0200

    Test of Turn

--- tictactoe.go
+++ tictactoe.go
@@ -1,12 +1,17 @@
 package tictactoe
 
 type Field struct{
+  positions [3][3] int
 }
 
 func NewField() Field {
-  return Field{}
+  return Field{ positions: [3][3]int{{0,0,0},{0,0,0},{0,0,0}}}
 }
 
-func (this *Field) isEmpty() bool{
-  return false
+func (this *Field) isGameFinished() bool{
+  return true
+}
+
+func (this *Field) Turn(x int, y int, z int) Field {
+  return Field{ positions: [3][3]int{{2,0,0},{0,0,0},{0,0,0}}}
 }
--- tictactoe_test.go
+++ tictactoe_test.go
@@ -8,6 +8,8 @@ import (
 func TestField(t *testing.T){
   Convey("Field", t, func() {
     field := NewField()
-    So(field.isEmpty(),ShouldBeTrue)
+    So(field.isGameFinished(),ShouldBeTrue)
+    So(field.Turn(0,0,2),ShouldResemble,Field{ positions: [3][3]int{{2,0,0},{0,0,0},{0,0,0}}})
+    So(field.Turn(0,0,2),ShouldNotResemble,Field{ positions: [3][3]int{{0,0,0},{0,0,0},{0,0,0}}})
   })
 }
