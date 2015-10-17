32313e9b0294e78725d12e7885ec4e0b39e5ac41
Kelyane <kelyane.lokita@gmail.com>
2015-10-17 22:37:35 +0200

    function is winner

--- tictactoe.go
+++ tictactoe.go
@@ -4,6 +4,13 @@ type Field struct{
   positions [3][3] int
 }
 
+type Player int
+
+const (
+        O Player = 0
+        X Player = 1
+)
+
 func NewField() Field {
   return Field{ positions: [3][3]int{{0,0,0},{0,0,0},{0,0,0}}}
 }
@@ -12,7 +19,19 @@ func (this *Field) isGameFinished() bool{
   return true
 }
 
-func (this *Field) Turn(x int, y int, z int) Field {
-  this.positions[x][y] = z
+func (this *Field) Turn(x int, y int, player Player) Field {
+  this.positions[x][y] = int(player)
   return *this
+}
+
+func (this *Field) isWinner(player Player) bool{
+  return (
+     (this.positions[0][0] == int(player) && this.positions[0][1] == int(player) && this.positions[0][2] == int(player)) || //First row horizontal
+     (this.positions[1][0] == int(player) && this.positions[1][1] == int(player) && this.positions[1][2] == int(player)) || //Second row horizontal
+     (this.positions[2][0] == int(player) && this.positions[2][1] == int(player) && this.positions[2][2] == int(player)) ||  //Third row horizontal
+     (this.positions[0][0] == int(player) && this.positions[1][0] == int(player) && this.positions[2][0] == int(player)) || //First column vertical
+     (this.positions[0][1] == int(player) && this.positions[1][1] == int(player) && this.positions[2][1] == int(player)) || //Second column vertical
+     (this.positions[0][2] == int(player) && this.positions[1][2] == int(player) && this.positions[2][2] == int(player)) || //Third column vertical
+     (this.positions[0][0] == int(player) && this.positions[1][1] == int(player) && this.positions[2][2] == int(player)) || //Main diagonal
+     (this.positions[0][2] == int(player) && this.positions[1][1] == int(player) && this.positions[2][0] == int(player)) )  //Oposite diagonal
 }
--- tictactoe_test.go
+++ tictactoe_test.go
@@ -11,5 +11,34 @@ func TestField(t *testing.T){
     So(field.isGameFinished(),ShouldBeTrue)
     So(field.Turn(0,0,2),ShouldResemble,Field{ positions: [3][3]int{{2,0,0},{0,0,0},{0,0,0}}})
     So(field.Turn(0,0,2),ShouldNotResemble,Field{ positions: [3][3]int{{0,0,0},{0,0,0},{0,0,0}}})
-  })
+    var player = 1;
+    fieldWinner := NewField()
+    fieldWinner = Field{ positions: [3][3]int{{1,1,1},{0,0,0},{0,0,0}}}
+    So(fieldWinner.isWinner(Player(player)),ShouldBeTrue)
+    fieldWinner = Field{ positions: [3][3]int{{0,0,0},{1,1,1},{0,0,0}}}
+    So(fieldWinner.isWinner(Player(player)),ShouldBeTrue)
+    fieldWinner = Field{ positions: [3][3]int{{0,0,0},{0,0,0},{1,1,1}}}
+    So(fieldWinner.isWinner(Player(player)),ShouldBeTrue)
+    fieldWinner = Field{ positions: [3][3]int{{0,0,2},{0,0,2},{0,0,2}}}
+    So(fieldWinner.isWinner(Player(player)),ShouldBeFalse)
+    fieldWinner = Field{ positions: [3][3]int{{1,0,0},{0,1,0},{0,0,1}}}
+    So(fieldWinner.isWinner(Player(player)),ShouldBeTrue)
+    fieldWinner = Field{ positions: [3][3]int{{0,0,1},{0,0,1},{0,0,2}}}
+    So(fieldWinner.isWinner(Player(player)),ShouldBeFalse)
+    fieldWinner = Field{ positions: [3][3]int{{0,1,1},{0,0,1},{1,0,2}}}
+    So(fieldWinner.isWinner(Player(player)),ShouldBeFalse)
+    player = 2;
+    fieldWinner = Field{ positions: [3][3]int{{0,0,0},{0,0,0},{1,1,1}}}
+    So(fieldWinner.isWinner(Player(player)),ShouldBeFalse)
+    fieldWinner = Field{ positions: [3][3]int{{2,0,0},{2,0,0},{2,0,0}}}
+    So(fieldWinner.isWinner(Player(player)),ShouldBeTrue)
+    fieldWinner = Field{ positions: [3][3]int{{0,2,0},{0,2,0},{0,2,0}}}
+    So(fieldWinner.isWinner(Player(player)),ShouldBeTrue)
+    fieldWinner = Field{ positions: [3][3]int{{0,0,2},{0,0,2},{0,0,2}}}
+    So(fieldWinner.isWinner(Player(player)),ShouldBeTrue)
+    fieldWinner = Field{ positions: [3][3]int{{0,0,2},{0,2,0},{2,0,0}}}
+    So(fieldWinner.isWinner(Player(player)),ShouldBeTrue)
+    fieldWinner = Field{ positions: [3][3]int{{0,0,0},{0,0,0},{1,2,1}}}
+    So(fieldWinner.isWinner(Player(player)),ShouldBeFalse)
+    })
 }
